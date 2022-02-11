41c8bb3636f352cc32fe5e4a47d82bdd9a1504aa
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-02-11 16:56:57 +0000

    Final commit before leaving work

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     acts_as_votable
     has_one_attached :image, dependent: :destroy
+    validates :title, :image, presence: true
 end
